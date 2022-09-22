102d0a3b398b580361b3b70e414c289ed16a2d4c
nawodyaishan <nawodyain@gmail.com>
2022-09-22 22:37:51 +0530

    Add PointOfInterestForCreationDto

---
+++ CityInfoService/CityInfoServiceAPI/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,7 @@
+namespace CityInfoServiceAPI.Models;
+
+public class PointOfInterestForCreationDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
